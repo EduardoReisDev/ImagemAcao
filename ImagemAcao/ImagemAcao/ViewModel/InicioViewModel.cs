bb4dfc6aab8fb231158027fa7735b26212067aa8
Eduardo Reis <eduardoreisdev@gmail.com>
2020-03-18 18:16:48 -0300

    18/03

--- ImagemAcao/ImagemAcao/ViewModel/InicioViewModel.cs
+++ ImagemAcao/ImagemAcao/ViewModel/InicioViewModel.cs
@@ -11,6 +11,8 @@ namespace ImagemAcao.ViewModel
     {
         public Jogo Jogo { get; set; }
         public Command IniciarCommand { get; set; }
+        private string _MsgErro;
+        public string MsgErro { get { return _MsgErro; } set { _MsgErro = value; OnPropertyChanged("MsgErro"); } }
 
         public InicioViewModel()
         {
@@ -18,13 +20,37 @@ namespace ImagemAcao.ViewModel
             Jogo = new Jogo();
             Jogo.Grupo1 = new Grupo();
             Jogo.Grupo2 = new Grupo();
+
+            Jogo.TempoPalavra = 60;
+            Jogo.Rodadas = 5;
+
         }
 
         public void IniciarJogo()
         {
-            Armazenamento.Armazenamento.Jogo = this.Jogo;
-            Armazenamento.Armazenamento.RodadaAtual = 1;
-            App.Current.MainPage = new View.Jogo(Jogo.Grupo1);
+            string error = "";
+
+            if (Jogo.TempoPalavra < 10)
+            {
+                error += "O tempo mínimo por rodada é 10 segundos.";
+            }
+
+            if(Jogo.Rodadas <= 0)
+            {
+                error += "\nO número mínimo de rodadas é 1.";
+            }
+
+            if(error.Length > 0)
+            {
+                MsgErro = error;
+            }
+
+            else
+            {
+                Armazenamento.Armazenamento.Jogo = this.Jogo;
+                Armazenamento.Armazenamento.RodadaAtual = 1;
+                App.Current.MainPage = new View.Jogo(Jogo.Grupo1);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
